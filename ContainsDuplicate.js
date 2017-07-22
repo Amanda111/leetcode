3ebd4edb549aa6c7295e9488a21462ee9b21d7cd
Amanda111 <631551773@qq.com>
2017-07-22 23:56:47 +0800

    easy level

---
+++ ContainsDuplicate.js
@@ -0,0 +1,21 @@
+//task217
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+var containsDuplicate = function(nums) {
+    var hash = {},
+        value,
+        i = 0;
+    while(i<nums.length){
+        value = nums[i];
+        if(value in hash){
+            return true;
+        }else{
+            hash[value] = "haha";
+        }
+        i++;
+    }
+    return false;
+};
---
+++ SymmetricTree.js
@@ -0,0 +1,50 @@
+//task101
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+//iteratively
+var isSymmetric = function(root) {
+   if(!root) return true;
+   var curr = [root.left,root.right],
+       sub = [],
+       nullnum = 0;
+   while(curr){
+       for(var i = 0;i<curr.length;i++){
+           if(curr[i] && curr[curr.length -i -1]){
+               if(curr[i].val == curr[curr.length - i -1].val){
+                   sub.push(curr[i].left,curr[i].right)
+               }else{
+                   return false;
+               }            
+           }else if(curr[i] || curr[curr.length - i - 1]){
+               return false;
+           }else{
+               nullnum ++;
+           }
+       }
+       if(nullnum == curr.length) return true;
+       curr = sub;
+       sub = [];
+       nullnum = 0;
+   }
+};
+
+//recursively
+var mirror = function(t1,t2){
+    if(t1 === null && t2 === null) return true;
+    if(t1 === null || t2 === null) return false;
+    return (t1.val == t2.val) && mirror(t1.left,t2.right) && mirror(t1.right,t2.left);
+};
+
+var isSymmetric = function(root) {
+   return mirror(root,root);
+};
